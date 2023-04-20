01730fb456117dd8e15e301b25beb57755d7ceae
Pawos-Howl <130191607+Pawos-Howl@users.noreply.github.com>
2023-04-20 08:41:03 -0700

    Minor Changes

--- JoinAndLeave.py
+++ JoinAndLeave.py
@@ -5,7 +5,6 @@ class JoinAndLeave(commands.Cog):
     def __init__(self, client):
         self.client: PawBot = client
 
-    # OS path of ENV "os.getenv('GUILD_JOIN_AND_LEAVE_CHANNEL_ID')"
     @commands.Cog.listener()
     async def on_member_join(self, member):
         await self.client.get_channel(self.JOIN_AND_LEAVE_CHANNEL).send(f"{member.name} has joined")
