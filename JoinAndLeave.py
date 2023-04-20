59f9e90edf62d983ec5b86a0e0dd53826e4f9e0a
Pawos-Howl <130191607+Pawos-Howl@users.noreply.github.com>
2023-04-20 07:37:15 -0700

    Minor Bug Fixes

--- JoinAndLeave.py
+++ JoinAndLeave.py
@@ -1,28 +1,18 @@
-import os
-#from discord.ext.commands.cog import Cog
 from discord.ext import commands
-
-# from bot import PawBot
-
-# client = PawBot()
-# def setup(client):
-#     client.add_cog(Cog(client))
+from bot import PawBot
 
 class JoinAndLeave(commands.Cog):
-    # OS path of ENV "os.getenv('GUILD_JOIN_AND_LEAVE_CHANNEL_ID')"
-    @commands.Cog.listener()
-    async def on_member_join(member):
-        await client.get_channel(os.getenv('JOIN_AND_LEAVE_CHANNEL')).send(f"{member.name} has joined")
-
-    @commands.Cog.listener()
-    async def on_member_remove(member):
-        await client.get_channel(os.getenv('JOIN_AND_LEAVE_CHANNEL')).send(f"{member.name} has left")
+    def __init__(self, client):
+        self.client: PawBot = client
 
     # OS path of ENV "os.getenv('GUILD_JOIN_AND_LEAVE_CHANNEL_ID')"
-    # @client.event
-    # async def on_member_join(member):
-    #     await client.get_channel(idchannel).send(f"{member.name} has joined")
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.client.get_channel(self.JOIN_AND_LEAVE_CHANNEL).send(f"{member.name} has joined")
 
-    # @client.event
-    # async def on_member_remove(member):
-    #     await client.get_channel(idchannel).send(f"{member.name} has left")
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        await self.client.get_channel(self.JOIN_AND_LEAVE_CHANNEL).send(f"{member.name} has left")
+
+async def setup(bot):
+    await bot.add_cog(JoinAndLeave(bot))
