885feb710546cc6b367783f2f6af397a79e3453b
Pawos-Howl <130191607+Pawos-Howl@users.noreply.github.com>
2023-04-19 20:48:21 -0700

    Delete puppyTimeRunner.py

--- puppyTimeRunner.py
+++
@@ -1,7 +0,0 @@
-import puppyTime
-print("Welcome to the test runner! This is just for test purposes to verify that the test works.")
-while True:
-    print("Enter the test command:")
-    req = input("")
-    output = puppyTime.time(req)
-    print(f'{output}\n')
