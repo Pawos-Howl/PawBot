cdd7a0dd463a6f525d2a1ab8a40e78bf5a56fd2f
Pawos-Howl <130191607+Pawos-Howl@users.noreply.github.com>
2023-04-19 20:33:34 -0700

    Minor Changes
    Added cogs and revised some code.

--- bot.py
+++ bot.py
@@ -1,33 +1,49 @@
-import discord
-import os
-# import logging
-from discord.ext.commands import Bot, CommandNotFound
-from discord.app_commands.errors import CommandInvokeError
+import os, discord
+from discord.ext.commands import Cog
+# The following two lines break my code on windows. use on Mac (\n for spaces): from dotenv import load_dotenv \n load_dotenv()
+from bot import PawBot
+from greetingCommands import Greetings
+from JoinAndLeave import JoinAndLeave
+from reactionRoles import verifiedRole
 
-#\u001b[38;5;82;1m
-# log = logging.getLogger("CaltrainAlerts.\u001b[38;5;82;1mBot\u001b[0m")
+TOKEN = os.getenv('DISCORD_TOKEN')
+async def setup(bot):
+    await bot.add_cog(Cog(bot))
+    await bot.add_cog(Greetings(bot))
+    await bot.add_cog(JoinAndLeave(bot))
+    await bot.add_cog(verifiedRole(bot))
+bot = PawBot()
 
-class PawBot(Bot):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.all(),command_prefix="!paw ") # Do not set the command prefix to / since that is the default for slash commands
-        self.MY_GUILD = discord.Object(id=int(os.getenv('DISCORD_GUILD'))) # Set this to the guild ID you want to use slash commands 
+@bot.event
+async def on_member_join(member):
+    channel = await member.create_dm() # Create the channel and set it to a variable instead
+    await channel.send(
+        f'Hewwo {member.name}! Welcome to the Pawos Howl Gang! This bot was coded by Paw (with some help from friends). Have fun and enjoy your stay! -Pawos Howl'
+        )
+    
+# The following lines do not work correctly. Error with needing to have both CTX and Message be first in the order
+#@bot.event
+#async def on_message(ctx, message: discord.message, *, member: discord.member):
+#    ## Bot triggering itself protection
+#    if message.author == bot.user: return
+#    elif message == 'hewwo':
+#        #await message.channel.send("hewwo")
+#        await ctx.channel.send(f"hewwo {member}~ How are ya?")
+#    # Catchall is not needed
+#    #await bot.process_commands(message)
 
-    async def on_ready(self): # All this function really needs to do is just say that its online, no need for any fancy stuff
-        print("Bot is online!")
+@bot.command()
+async def hewwo(ctx, member: discord.member):
+    await ctx.send(f'Hewwo {member}! How are you doing?')
 
-    async def on_command_error(self, ctx, ex):
-        print(f"main.on_command_error: {type(ex)}")
-        if isinstance(ex, CommandNotFound): return
-        if ctx.author.id == 979210001556070491: 
-            err_str = f"`{getattr(ex, '__module__')}:  {ex.args[0]}`"
-            await ctx.reply(err_str)
-        if isinstance(ex, CommandInvokeError):
-            #log.exception(ex.__cause__)
-            raise ex.__cause__
-        else:
-            #log.exception(ex)
-            raise ex.__cause__
+@bot.command()
+async def generalTest(ctx):
+    await ctx.send('STANDARD TEST:\nRETURNED TRUE. TEST SUCESSFUL.')
 
-    async def setup_hook(self):
-        self.tree.copy_global_to(guild=self.MY_GUILD)
-        await self.tree.sync(guild=self.MY_GUILD)
+@bot.tree.command()
+async def info(interaction: discord.Interaction, member: discord.Member): # This is how you do slash commands; switched ctx for interaction
+    """Tells you some info about the member."""
+    msg = f'{member} joined on {member.joined_at} and has {len(member.roles)} roles.'
+    await interaction.response.send_message(msg) # Instead of ctx.send, use interaction.response.send_message
+
+bot.run(TOKEN) # This ALWAYS is at the bottom of the file no matter what
