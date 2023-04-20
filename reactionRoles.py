4abb2f60f0361d4cbdeba31844624c6810477c93
Pawos-Howl <130191607+Pawos-Howl@users.noreply.github.com>
2023-04-19 21:33:19 -0700

    Minor Changes

--- reactionRoles.py
+++ reactionRoles.py
@@ -1,11 +1,12 @@
 import discord, os
-from discord.ext.commands.cog import Cog
+#from discord.ext.commands.cog import Cog
 from discord.ext import commands
-from bot import PawBot
 
-client = PawBot()
-def setup(client):
-    client.add_cog(Cog(client))
+# from bot import PawBot
+
+# client = PawBot()
+# def setup(client):
+#     client.add_cog(Cog(client))
 
 class verifiedRole(commands.Cog):
     #Sauce of some of this amazing code: https://stackoverflow.com/questions/52210855/give-role-when-a-user-add-reaction-discord-py
@@ -24,7 +25,7 @@ class verifiedRole(commands.Cog):
         Channel = client.get_channel(os.getenv('VERIFIED_CHANNEL'))
         Moji = await Channel.send("TEXT")
         await Moji.add_reaction('✅')
-    @client.event
+    @commands.Cog.event
     async def on_reaction_add(reaction, user):
         Channel = client.get_channel(os.getenv('VERIFIED_CHANNEL'))
         if reaction.message.channel.id != Channel.id:
@@ -32,7 +33,7 @@ class verifiedRole(commands.Cog):
         if reaction.emoji == "✅":
             Role = discord.utils.get(user.server.roles, name="YOUR_ROLE_NAME_HERE")
             await user.add_roles(Role)
-    @client.event
+    @commands.Cog.event
     async def on_reaction_remove(reaction, user):
         Channel = client.get_channel(os.getenv('VERIFIED_CHANNEL'))
         if reaction.message.channel.id != Channel.id:
