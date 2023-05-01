65f1d54b17076761c989f82c227f38fd54515437
Pawos-Howl <pawoshowl@gmail.com>
2023-04-30 20:05:07 -0700

    Minor Correction

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ class PawBot(Bot):
         await self.load_extension('info')
         await self.load_extension('rollDice')
         await self.load_extension('affection')
-        #await self.load_extension('botDisable') #NOT WORKING!!!
+        await self.load_extension('botDisable')
         #Leave at the bottom
         self.tree.copy_global_to(guild=self.MY_GUILD)
         await self.tree.sync(guild=self.MY_GUILD)
