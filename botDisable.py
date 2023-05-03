674f4664c4783d36f95cd3548ece6a54a3c6c496
Pawos-Howl <pawoshowl@gmail.com>
2023-05-03 09:47:44 -0700

    Fixed Stop

--- botDisable.py
+++ botDisable.py
@@ -1,4 +1,4 @@
-import discord
+import discord, sys
 from discord.ext import commands
 from bot import PawBot
 
@@ -10,11 +10,9 @@ class botDisable(commands.Cog):
     @commands.command()
     async def killBot(self, ctx):
         if discord.Object(id=int(ctx.author.id)) == self.client.MY_USER_ID:
-            await ctx.send("PawBot will now try to stop itself.")
-            try:
-                quit()
-            except:
-                await ctx.send("There has been an error. Pawbot gets to live. >:3")
+            # This will cause many errors on the discord package, but it works. "https://www.scaler.com/topics/how-to-end-program-in-python/" if I want to fix it
+            await ctx.send("Terminating...")
+            sys.exit("Terminating")
         else:
             await ctx.send('>:c\nYou are not allowed to do that!')
 
